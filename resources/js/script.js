7b9563f0f19730fba1ade2fb306b1f2d236e9742
ANKITSINGH9472 <42248048+ANKITSINGH9472@users.noreply.github.com>
2020-03-31 09:53:18 +0530

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $('.js--section-text').waypoint(function(direction){
+        if (direction == "down")
+            {
+                $('nav').addClass('sticky');
+            } else{
+                $('nav').removeClass('sticky');
+            }
+    },{
+        offset: '60px;'
+    });
+    /*-----scrolling navigtion---*/
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    // animation to scroll
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    // mobile-navigation
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('fas fa-bars')) {
+            icon.addClass('fas fa-bars');
+            icon.removeClass('fas fa-bars');
+        } else {
+            icon.addClass('fas fa-bars');
+            icon.removeClass('fas fa-bars');
+        }        
+    });
+    
+}); 
